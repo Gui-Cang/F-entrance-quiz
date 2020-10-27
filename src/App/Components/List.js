0e55e2901b3dcd624464939797ee0db08fd5a2a2
Gui-Cang <821192514@qq.com>
2020-10-28 00:08:12 +0800

    [dingke] refactor: try to display them on page by refactoring List.js, but failed.

--- src/App/Components/List.js
+++ src/App/Components/List.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
-const List = () => <h1>list from back-end</h1>;
-
 const urlList = 'http://localhost:8080/students';
-fetch(urlList)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: {}
+    };
+  };
+
+  componentDidMount() {
+    this.getStudentList();
+  }
+
+  getStudentList = () => {
+    fetch(urlList)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (<div>
+      <h2>list from back-end</h2>
+      <p>{this.getStudentList()}</p>
+    </div>);
+  }
+}
 
 export default List;
