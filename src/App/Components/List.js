d2479f2492fdcfb27ce9578c38ef3a2fe09f38f8
Gui-Cang <821192514@qq.com>
2020-10-27 23:06:04 +0800

    [dingke] feat: fetch data from back-end successfully, and then try to display them on page.

--- src/App/Components/List.js
+++ src/App/Components/List.js
@@ -2,4 +2,16 @@ import React from 'react';
 
 const List = () => <h1>list from back-end</h1>;
 
+const urlList = 'http://localhost:8080/students';
+fetch(urlList)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 export default List;
